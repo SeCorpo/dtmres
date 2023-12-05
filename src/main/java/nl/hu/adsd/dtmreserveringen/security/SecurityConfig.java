b362a2e0a8339bc7f8618d701a95bf6e93c00095
Timmevbeek <timme.vanbeek@student.hu.nl>
2023-12-05 14:15:53 +0100

    added security config, commented out some if statements

---
+++ src/main/java/nl/hu/adsd/dtmreserveringen/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package nl.hu.adsd.dtmreserveringen.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((reqeust) -> reqeust
+                    .requestMatchers("/api/login/**").permitAll()
+                    .anyRequest().permitAll()
+                )
+                .formLogin((form) -> form
+                    .disable()
+                ).csrf((csrf) -> csrf.disable());
+
+        return http.build();     
+    }
+}

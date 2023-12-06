0d06a3ed8d927266a8d2f9cbd4d060dd1d494dc6
Timme van Beek <Timme.vanbeek@student.hu.nl>
2023-12-06 14:05:19 +0100

    some small changes made to securityconfig with a small amount of succes

--- src/main/java/nl/hu/adsd/dtmreserveringen/security/SecurityConfig.java
+++ src/main/java/nl/hu/adsd/dtmreserveringen/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package nl.hu.adsd.dtmreserveringen.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,13 +13,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .securityMatcher("/api/**")
             .authorizeHttpRequests((reqeust) -> reqeust
-                    .requestMatchers("/api/login/**").permitAll()
+                    .requestMatchers(HttpMethod.GET,"/reservation/**").permitAll()
+                    .requestMatchers("/product/**").permitAll()
                     .anyRequest().permitAll()
                 )
                 .formLogin((form) -> form
                     .disable()
-                ).csrf((csrf) -> csrf.disable());
+                )
+                .csrf((csrf) -> csrf
+                    .disable()
+                );
 
         return http.build();     
     }

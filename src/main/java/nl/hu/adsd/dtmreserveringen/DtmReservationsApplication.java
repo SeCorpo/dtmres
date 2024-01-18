48a4d913e3ca404f279c57de2918e59ec4689376
Timmevbeek <timme.vanbeek@student.hu.nl>
2024-01-18 09:39:02 +0100

    attemp number 2

--- src/main/java/nl/hu/adsd/dtmreserveringen/DtmReservationsApplication.java
+++ src/main/java/nl/hu/adsd/dtmreserveringen/DtmReservationsApplication.java
@@ -2,9 +2,8 @@ package nl.hu.adsd.dtmreserveringen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class DtmReservationsApplication {
 
 	public static void main(String[] args) {
